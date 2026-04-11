cdb1c5d4a43187a878435d2f4ce1aa6595454704
nkuanysh <nurik.2004703@gmail.com>
2026-04-11 17:36:37 +0500

    Authentication

--- app/src/main/java/com/example/ozmade/OzMadeApplication.kt
+++ app/src/main/java/com/example/ozmade/OzMadeApplication.kt
@@ -1,7 +1,19 @@
 package com.example.ozmade
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class OzMadeApplication : Application()
+class OzMadeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            PlayIntegrityAppCheckProviderFactory.getInstance()
+        )
+    }
+}
